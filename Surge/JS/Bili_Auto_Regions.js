2d3e15662fb6e64b7da94b9269cf21c049925bdd
Arthur-Edward <54299332+Arthur-Edward@users.noreply.github.com>
2022-05-13 11:47:31 +0800

    Update Bili_Auto_Regions.js

--- Surge/JS/Bili_Auto_Regions.js
+++ Surge/JS/Bili_Auto_Regions.js
@@ -81,7 +81,7 @@ async function SwitchRegion(play) {
 	const TW = $.read('BiliArea_TW') || 'BiliRegions'; //Your Taiwan sub-policy name.
 	const HK = $.read('BiliArea_HK') || 'BiliRegions'; //Your HongKong sub-policy name.
 	const DF = $.read('BiliArea_DF') || 'BiliRegions'; //Sub-policy name used after region is blocked(e.g. url 404)
-	const off = $.read('BiliArea_disabled') || ''; //WiFi blacklist(disable region change), separated by commas.
+	const off = $.read('BiliArea_disabled') || 'BiliRegions'; //WiFi blacklist(disable region change), separated by commas.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
 		let select;
